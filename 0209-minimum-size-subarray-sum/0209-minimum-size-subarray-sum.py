3d7e2dac5bd8544fc683b0543dacdceca880da8a
Arpit Kumar <158032263+arpitk2509@users.noreply.github.com>
2026-06-30 00:50:36 +0530

    Time: 19 ms (15.86%), Space: 30.7 MB (16.04%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -9,4 +9,4 @@ class Solution:
                 min_length=min(min_length, r-l+1)
                 current-=nums[l] 
                 l+=1
-        return 0 if min_length == float('inf') else min_length
+        return min_length if min_length != float('inf') else 0
